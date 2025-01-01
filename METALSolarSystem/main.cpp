6b2ddcacbb27b412cd3b10e1f7c37d4e2ac2b693
Jacky Lei <jackylei27@gmail.com>
2025-01-01 10:11:40 -0500

    Initial Commit

---
+++ METALSolarSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  METALSolarSystem
+//
+//  Created by Jacky Lei on 1/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
